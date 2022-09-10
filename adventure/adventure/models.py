18449351546d66b1f35678a4dd719ab5877a9ef6
simalpas <simalpas@gmail.com>
2022-09-10 14:02:17 +0100

    addendum to previous, models done

---
+++ adventure/adventure/models.py
@@ -0,0 +1,72 @@
+from django.db import models
+import uuid
+
+
+class Object(models.Model):
+    uuid = models.UUIDField(primary_key=True,
+                            default=uuid.uuid4,
+                            editable=False)
+    name = models.CharField(
+        max_length=100,
+        null=False,
+    )
+    description = models.CharField(max_length=255, blank=False)
+    closerInspection = models.CharField(
+        max_length=255, default='There is nothing of further interest here')
+
+
+class Room(Object):
+    # A location with a exits to the north, south, east, west, up and down directions
+    north = models.ForeignKey('self',
+                              on_delete=models.SET_NULL,
+                              null=True,
+                              related_name='goNorth')
+    south = models.ForeignKey('self',
+                              on_delete=models.SET_NULL,
+                              null=True,
+                              related_name='goSouth')
+    east = models.ForeignKey('self',
+                             on_delete=models.SET_NULL,
+                             null=True,
+                             related_name='goEast')
+    west = models.ForeignKey('self',
+                             on_delete=models.SET_NULL,
+                             null=True,
+                             related_name='goWest')
+    up = models.ForeignKey('self',
+                           on_delete=models.SET_NULL,
+                           null=True,
+                           related_name='goUp')
+    down = models.ForeignKey('self',
+                             on_delete=models.SET_NULL,
+                             null=True,
+                             related_name='goDown')
+
+
+class Item(Object):
+    location = models.ForeignKey('Room', on_delete=models.SET_NULL, null=True)
+    heldBy = models.ForeignKey('Person', on_delete=models.SET_NULL, null=True)
+    weight = models.IntegerField(default=0)
+    SIZE_CHOICES = [
+        ('sm', 'Small'),
+        ('md', 'Medium'),
+        ('lg', 'Large'),
+        ('xl', 'Large'),
+        ('en', 'Enormous'),
+    ]
+    size = models.CharField(max_length=2, choices=SIZE_CHOICES, default='sm')
+
+
+class Person(models.Model):
+    uuid = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        null=False,
+        blank=False,
+    )
+    name = models.CharField(
+        max_length=100,
+        blank=False,
+        null=False,
+    )
+    location = models.ForeignKey('Room', on_delete=models.SET_NULL, null=True)
